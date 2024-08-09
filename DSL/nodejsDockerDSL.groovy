9aa1fef0c23556c6e4a818d565577fd14524833e
vlmnst <vale-mansueto@hotmail.com>
2024-08-09 11:23:38 -0300

    modifico credenciales

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/vlmnst-betech/nodejsapp.git', 'main') { node ->
+            node / gitConfigName('vlmnst-betech')
+            node / gitConfigEmail('vmanuseto.betech@gmail.com')
         }
     }
     triggers {
@@ -14,9 +14,9 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('vlmnst-betech/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('vmansuetobetech')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
