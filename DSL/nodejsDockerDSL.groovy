61ab807844e41f4e1fec32b17158643033317b6c
vlmnst-betech <122399157+vlmnst-betech@users.noreply.github.com>
2024-08-13 16:49:20 -0300

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('vlmnst-betech/nodejsapp')
+            repositoryName('vmansuetobetech/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('vmansuetobetech')
             forcePull(false)
